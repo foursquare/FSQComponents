68a86377658be76da21fc4e72fafae840a142334
Cameron Mulhern <cameron@foursquare.com>
2015-04-16 11:47:41 -0400

    Adds FSQComponentButton

---
+++ FSQComponents/FSQComponents/FSQComponentButton.h
@@ -0,0 +1,41 @@
+//
+//  FSQComponentButton.h
+//  iOS Example
+//
+//  Created by Cameron Mulhern on 4/14/15.
+//  Copyright (c) 2015 Cameron Mulhern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FSQComposable.h"
+
+@interface FSQComponentButtonModel : NSObject
+
+@property (nonatomic) UIFont *font;
+@property (nonatomic) UIColor *backgroundColor;
+@property (nonatomic) CGFloat cornerRadius;
+
+- (instancetype)initWithImage:(UIImage *)image title:(NSString *)title titleColor:(UIColor *)titleColor;
+
+- (void)setImage:(UIImage *)image forState:(UIControlState)state;
+- (void)setTitle:(NSString *)title forState:(UIControlState)state;
+- (void)setTitleColor:(UIColor *)color forState:(UIControlState)state;
+
+- (UIImage *)imageForState:(UIControlState)state;
+- (NSString *)titleForState:(UIControlState)state;
+- (UIColor *)titleColorForState:(UIControlState)state;
+
+- (void)addTarget:(id)target action:(SEL)action forControlEvents:(UIControlEvents)controlEvents;
+- (void)removeTarget:(id)target action:(SEL)action forControlEvents:(UIControlEvents)controlEvents;
+
+@end
+
+@interface UIButton (FSQComponentButton) <FSQComposable>
+
+- (void)configureWithViewModel:(FSQComponentButtonModel *)model;
+
++ (CGSize)sizeForViewModel:(FSQComponentButtonModel *)model constrainedToSize:(CGSize)constrainedToSize;
++ (CGSize)estimatedSizeForViewModel:(FSQComponentButtonModel *)model constrainedToSize:(CGSize)constrainedToSize;
+
+@end
---
+++ FSQComponents/FSQComponents/FSQComponentButton.m
@@ -0,0 +1,268 @@
+//
+//  FSQComponentButton.m
+//  iOS Example
+//
+//  Created by Cameron Mulhern on 4/14/15.
+//  Copyright (c) 2015 Cameron Mulhern. All rights reserved.
+//
+
+#import "FSQComponentButton.h"
+#import <objc/runtime.h>
+
+#pragma mark - FSQComponentButtonModel -
+
+@interface FSQTargetSelectorPair : NSObject
+
+@property (nonatomic, readonly, unsafe_unretained) id target;
+@property (nonatomic, readonly) SEL selector;
+
+@end
+
+@implementation FSQTargetSelectorPair
+
+- (instancetype)initWithTarget:(id)target selector:(SEL)selector {
+    if ((self = [super init])) {
+        _target = target;
+        _selector = selector;
+    }
+    return self;
+}
+
+@end
+
+@interface FSQComponentButtonModel ()
+
+@property (nonatomic) NSMutableDictionary *imageDictionary;
+@property (nonatomic) NSMutableDictionary *titleDictionary;
+@property (nonatomic) NSMutableDictionary *titleColorDictionary;
+
+@property (nonatomic) NSMutableDictionary *actionsDictionary;
+
+@end
+
+@implementation FSQComponentButtonModel
+
+- (instancetype)init {
+    return [self initWithImage:nil title:nil titleColor:nil];
+}
+
+- (instancetype)initWithImage:(UIImage *)image title:(NSString *)title titleColor:(UIColor *)titleColor {
+    if ((self = [super init])) {
+        self.imageDictionary = [[NSMutableDictionary alloc] init];
+        self.titleDictionary = [[NSMutableDictionary alloc] init];
+        self.titleColorDictionary = [[NSMutableDictionary alloc] init];
+        self.actionsDictionary = [[NSMutableDictionary alloc] init];
+        
+        [self setImage:image forState:UIControlStateNormal];
+        [self setTitle:title forState:UIControlStateNormal];
+        [self setTitleColor:titleColor forState:UIControlStateNormal];
+    }
+    return self;
+}
+
+- (void)setImage:(UIImage *)image forState:(UIControlState)state {
+    if (image) {
+        self.imageDictionary[@(state)] = image;
+    }
+    else {
+        [self.imageDictionary removeObjectForKey:@(state)];
+    }
+}
+
+- (void)setTitle:(NSString *)title forState:(UIControlState)state {
+    if (title) {
+        self.titleDictionary[@(state)] = title;
+    }
+    else {
+        [self.titleDictionary removeObjectForKey:@(state)];
+    }
+}
+
+- (void)setTitleColor:(UIColor *)color forState:(UIControlState)state {
+    if (color) {
+        self.titleColorDictionary[@(state)] = color;
+    }
+    else {
+        [self.titleColorDictionary removeObjectForKey:@(state)];
+    }
+}
+
+- (UIImage *)imageForState:(UIControlState)state {
+    return self.imageDictionary[@(state)];
+}
+
+- (NSString *)titleForState:(UIControlState)state {
+    return self.titleDictionary[@(state)];
+}
+
+- (UIColor *)titleColorForState:(UIControlState)state {
+    return self.titleColorDictionary[@(state)];
+}
+
+- (void)addTarget:(id)target action:(SEL)action forControlEvents:(UIControlEvents)controlEvents {
+    FSQTargetSelectorPair *targetSelector = [[FSQTargetSelectorPair alloc] initWithTarget:target selector:action];
+    
+    for (NSNumber *controlEvent in [[self class] allControlEvents]) {
+        if ((controlEvents & [controlEvent unsignedIntegerValue]) != 0) {
+            NSMutableArray *actions = self.actionsDictionary[controlEvent];
+            if (actions == nil) {
+                actions = [[NSMutableArray alloc] init];
+                self.actionsDictionary[controlEvent] = actions;
+            }
+            
+            [actions addObject:targetSelector];
+        }
+    }
+}
+
+- (void)removeTarget:(id)target action:(SEL)action forControlEvents:(UIControlEvents)controlEvents {
+    for (NSNumber *controlEvent in [[self class] allControlEvents]) {
+        if ((controlEvents & [controlEvent unsignedIntegerValue]) != 0) {
+            NSMutableArray *actions = self.actionsDictionary[@(controlEvents)];
+            NSMutableIndexSet *indexSet = [[NSMutableIndexSet alloc] init];
+            
+            [actions enumerateObjectsUsingBlock:^(FSQTargetSelectorPair *targetSelector, NSUInteger index, BOOL *stop) {
+                if (targetSelector.target == target && (action == NULL || targetSelector.selector == action)) {
+                    [indexSet addIndex:index];
+                }
+            }];
+            
+            [actions removeObjectsAtIndexes:indexSet];
+        }
+    }
+}
+
+#pragma mark - Class methods
+
++ (NSArray *)allControlStates {
+    static dispatch_once_t predicate;
+    static NSArray *controlStates;
+    dispatch_once(&predicate, ^() {
+        controlStates = @[
+            @(UIControlStateNormal),
+            @(UIControlStateHighlighted),
+            @(UIControlStateDisabled),
+            @(UIControlStateSelected),
+            @(UIControlStateApplication)
+        ];
+    });
+    return controlStates;
+}
+
++ (NSArray *)allControlEvents {
+    static dispatch_once_t predicate;
+    static NSArray *controlEvents;
+    dispatch_once(&predicate, ^() {
+        controlEvents = @[
+            @(UIControlEventTouchDown),
+            @(UIControlEventTouchDownRepeat),
+            @(UIControlEventTouchDragInside),
+            @(UIControlEventTouchDragOutside),
+            @(UIControlEventTouchDragEnter),
+            @(UIControlEventTouchDragExit),
+            @(UIControlEventTouchUpInside),
+            @(UIControlEventTouchUpOutside),
+            @(UIControlEventTouchCancel),
+            @(UIControlEventValueChanged),
+            @(UIControlEventEditingDidBegin),
+            @(UIControlEventEditingChanged),
+            @(UIControlEventEditingDidEnd),
+            @(UIControlEventEditingDidEndOnExit),
+            @(UIControlEventApplicationReserved)
+        ];
+    });
+    return controlEvents;
+}
+
+@end
+
+#pragma mark - UIButton (FSQComponentButton) -
+
+@interface UIButton (FSQComponentButtonPrivate)
+
+@property (nonatomic) FSQComponentButtonModel *fsqComponentsModel;
+
+@end
+
+@implementation UIButton (FSQComponentButton)
+
+- (void)configureWithViewModel:(FSQComponentButtonModel *)model {
+    self.fsqComponentsModel = model;
+    
+    self.titleLabel.font = model.font;
+    self.backgroundColor = model.backgroundColor;
+    self.layer.cornerRadius = model.cornerRadius;
+    
+    for (NSNumber *controlState in model.imageDictionary.allKeys) {
+        UIControlState state = [controlState unsignedIntegerValue];
+        [self setImage:[model imageForState:state] forState:state];
+    }
+    
+    for (NSNumber *controlState in model.titleDictionary.allKeys) {
+        UIControlState state = [controlState unsignedIntegerValue];
+        [self setTitle:[model titleForState:state] forState:state];
+    }
+    
+    for (NSNumber *controlState in model.titleColorDictionary.allKeys) {
+        UIControlState state = [controlState unsignedIntegerValue];
+        [self setTitleColor:[model titleColorForState:state] forState:state];
+    }
+    
+    for (NSNumber *controlEvent in model.actionsDictionary.allKeys) {
+        UIControlEvents events = [controlEvent unsignedIntegerValue];
+        NSArray *actions = model.actionsDictionary[controlEvent];
+        for (FSQTargetSelectorPair *targetSelector in actions) {
+            [self addTarget:targetSelector.target action:targetSelector.selector forControlEvents:events];
+        }
+    }
+}
+
+- (void)prepareForReuse {
+    for (NSNumber *controlState in [[self.fsqComponentsModel class] allControlStates]) {
+        UIControlState state = [controlState unsignedIntegerValue];
+        [self setImage:nil forState:state];
+        [self setTitle:nil forState:state];
+        [self setTitleColor:nil forState:state];
+    }
+    
+    [self removeTarget:nil action:NULL forControlEvents:UIControlEventAllEvents];
+    
+    self.fsqComponentsModel = nil;
+}
+
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
+    
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    
+}
+
+#pragma mark - Private
+
+- (void)setFsqComponentsModel:(FSQComponentButtonModel *)fsqComponentsModel {
+    return objc_setAssociatedObject(self, @selector(fsqComponentsModel), fsqComponentsModel, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (FSQComponentButtonModel *)fsqComponentsModel {
+    return objc_getAssociatedObject(self, @selector(fsqComponentsModel));
+}
+
+#pragma mark - Class methods
+
++ (CGSize)sizeForViewModel:(FSQComponentButtonModel *)model constrainedToSize:(CGSize)constrainedToSize {
+    static dispatch_once_t predicate;
+    static UIButton *buttonForSizing;
+    dispatch_once(&predicate, ^() {
+        buttonForSizing = [[UIButton alloc] initWithFrame:CGRectZero];
+    });
+    
+    [buttonForSizing configureWithViewModel:model];
+    return [buttonForSizing sizeThatFits:constrainedToSize];
+}
+
++ (CGSize)estimatedSizeForViewModel:(FSQComponentButtonModel *)model constrainedToSize:(CGSize)constrainedToSize {
+    return [self sizeForViewModel:model constrainedToSize:constrainedToSize];
+}
+
+@end
