89e3aaaa456c306c42dd32c09ae9744535f82cc7
Cameron Mulhern <cameron@foursquare.com>
2015-05-08 16:26:39 -0400

    Adds label layout example variant

--- iOS Example/iOS Example/LayoutExampleViewController.m
+++ iOS Example/iOS Example/LayoutExampleViewController.m
@@ -9,13 +9,22 @@
 #import "LayoutExampleViewController.h"
 
 #import "ComponentsCell.h"
-#import "FSQComponentButton.h"
+#import "FSQComponentButtonModel.h"
+#import "FSQComponentLabelModel.h"
 #import "FSQComponentsView.h"
+
+typedef NS_ENUM(NSUInteger, ContentMode) {
+    ContentModeButtons,
+    ContentModeLabels
+};
 
 @interface LayoutExampleViewController () <UITableViewDataSource, UITableViewDelegate>
 
+@property (nonatomic) UISegmentedControl *segmentedControl;
+@property (nonatomic) UIView *tableViewHeader;
 @property (nonatomic) UITableView *tableView;
 
+@property (nonatomic) ContentMode contentMode;
 @property (nonatomic) FSQComponentsViewModel *viewModel;
 
 @end
@@ -31,6 +40,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
     self.tableView = [[UITableView alloc] initWithFrame:CGRectZero];
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
@@ -41,11 +52,25 @@
     
     [self.tableView registerClass:[ComponentsCell class] forCellReuseIdentifier:@"Cell"];
     
+    self.tableViewHeader = [[UIView alloc] initWithFrame:CGRectZero];
+    self.tableView.tableHeaderView = self.tableViewHeader;
+    
+    NSArray *items = @[@"Buttons", @"Labels"];
+    self.segmentedControl = [[UISegmentedControl alloc] initWithItems:items];
+    self.segmentedControl.selectedSegmentIndex = 0;
+    [self.segmentedControl addTarget:self action:@selector(segmentedControlChanged) forControlEvents:UIControlEventValueChanged];
+    [self.tableViewHeader addSubview:self.segmentedControl];
+    
     [self loadContent];
 }
 
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
+    CGSize size = [self.segmentedControl sizeThatFits:self.view.bounds.size];
+    self.tableViewHeader.frame = CGRectMake(0.0, 0.0, self.view.frame.size.width, size.height + 10.0);
+    self.segmentedControl.frame = CGRectMake(10.0, 10.0, self.tableViewHeader.frame.size.width - 20.0, size.height);
+    self.tableView.tableHeaderView = self.tableViewHeader;
+    
     self.tableView.frame = self.view.bounds;
 }
 
@@ -54,20 +79,46 @@
     
     NSMutableArray *specifications = [[NSMutableArray alloc] init];
     for (NSString *token in [loremIpsum componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]) {
-        FSQComponentButtonModel *model = [[FSQComponentButtonModel alloc] initWithImage:nil title:token titleColor:[UIColor whiteColor]];
-        model.font = [UIFont systemFontOfSize:12.0];
-        model.backgroundColor = [UIColor blueColor];
-        model.cornerRadius = 3.0;
-        model.horizontalPadding = 10.0;
-        
-        FSQComponentSpecification *specification = [[FSQComponentSpecification alloc] initWithViewModel:model viewClass:[UIButton class]];
-        specification.layoutType = FSQComponentLayoutTypeFixed;
-        specification.widthConstraint = [UIButton sizeForViewModel:model constrainedToSize:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)].width;
-        [specifications addObject:specification];
+        switch (self.contentMode) {
+            case ContentModeButtons:
+                [specifications addObject:[self buttonComponentSpecificationForToken:token]];
+                break;
+            case ContentModeLabels:
+                [specifications addObject:[self labelComponentSpecificationForToken:token]];
+                break;
+        }
     }
     
     self.viewModel = [[FSQComponentsViewModel alloc] initWithComponentSpecifications:specifications];
     [self.tableView reloadData];
+}
+
+- (FSQComponentSpecification *)buttonComponentSpecificationForToken:(NSString *)token {
+    FSQComponentButtonModel *model = [[FSQComponentButtonModel alloc] initWithImage:nil title:token titleColor:[UIColor whiteColor]];
+    model.font = [UIFont systemFontOfSize:12.0];
+    model.backgroundColor = [UIColor blueColor];
+    model.cornerRadius = 3.0;
+    model.horizontalPadding = 10.0;
+    
+    FSQComponentSpecification *specification = [[FSQComponentSpecification alloc] initWithViewModel:model viewClass:[UIButton class]];
+    specification.layoutType = FSQComponentLayoutTypeFixed;
+    specification.widthConstraint = [UIButton sizeForViewModel:model constrainedToSize:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)].width;
+    return specification;
+}
+
+- (FSQComponentSpecification *)labelComponentSpecificationForToken:(NSString *)token {
+    FSQComponentLabelModel *model = [[FSQComponentLabelModel alloc] initWithText:token];
+    model.textColor = [UIColor blackColor];
+    
+    FSQComponentSpecification *specification = [[FSQComponentSpecification alloc] initWithViewModel:model viewClass:[UILabel class]];
+    specification.layoutType = FSQComponentLayoutTypeFixed;
+    specification.widthConstraint = [UILabel sizeForViewModel:model constrainedToSize:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)].width;
+    return specification;
+}
+
+- (void)segmentedControlChanged {
+    self.contentMode = (self.segmentedControl.selectedSegmentIndex == 0) ? ContentModeButtons : ContentModeLabels;
+    [self loadContent];
 }
 
 #pragma mark - UITableViewDataSource
